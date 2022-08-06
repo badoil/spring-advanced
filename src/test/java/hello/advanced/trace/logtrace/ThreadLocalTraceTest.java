e2cabaaced7236ab3c3adfb8366c252897ba94ce
oneal.oh <oneal.oh@kakaomobility.com>
2022-08-06 15:00:53 +0900

    update: ThreadLocalLogTrace

--- src/test/java/hello/advanced/trace/logtrace/ThreadLocalTraceTest.java
+++ src/test/java/hello/advanced/trace/logtrace/ThreadLocalTraceTest.java
@@ -5,13 +5,15 @@ import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 
 @Slf4j
-public class FieldLogTraceTest {
-    FieldLogTrace trace = new FieldLogTrace();
+public class ThreadLocalTraceTest {
+    ThreadLocalLogTrace trace = new ThreadLocalLogTrace();
 
     @Test
     void begin_end() {
         TraceStatus status1 = trace.begin("hello1");
         TraceStatus status2 = trace.begin("hello2");
+        TraceStatus status3 = trace.begin("hello3");
+        trace.end(status3);
         trace.end(status2);
         trace.end(status1);
     }
