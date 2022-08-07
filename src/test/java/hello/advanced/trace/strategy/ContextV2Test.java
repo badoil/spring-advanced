bd53d8e51cdf2f06437fb983475dc699d0a7797d
oneal.oh <oneal.oh@kakaomobility.com>
2022-08-07 15:26:40 +0900

    update: ContextV2 / Test

---
+++ src/test/java/hello/advanced/trace/strategy/ContextV2Test.java
@@ -0,0 +1,52 @@
+package hello.advanced.trace.strategy;
+
+import hello.advanced.trace.strategy.code.strategy.ContextV2;
+import hello.advanced.trace.strategy.code.strategy.Strategy;
+import hello.advanced.trace.strategy.code.strategy.StrategyLogic1;
+import hello.advanced.trace.strategy.code.strategy.StrategyLogic2;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+
+@Slf4j
+public class ContextV2Test {
+    /**
+     *전략 패턴 적용
+     * 클라이언트는 Context 를 실행하는 시점에 원하는 Strategy 를 전달
+     */
+    @Test
+    void strategyV1() {
+        ContextV2 context = new ContextV2();
+        context.execute(new StrategyLogic1());
+        context.execute(new StrategyLogic2());
+    }
+
+    /**
+     * 전략 패턴 익명 내부 클래스
+     * 익명함수
+     */
+    @Test
+    void strategyV2() {
+        ContextV2 context = new ContextV2();
+        context.execute(new Strategy() {
+            @Override
+            public void call() {
+                log.info("비즈니스 로직1 실행"); }
+        });
+        context.execute(new Strategy() {
+            @Override
+            public void call() {
+                log.info("비즈니스 로직2 실행"); }
+        });
+    }
+
+    /**
+     * 전략 패턴 익명 내부 클래스
+     * 람다
+     */
+    @Test
+    void strategyV3() {
+        ContextV2 context = new ContextV2();
+        context.execute(() -> log.info("비즈니스 로직1 실행"));
+        context.execute(() -> log.info("비즈니스 로직2 실행"));
+    }
+}
