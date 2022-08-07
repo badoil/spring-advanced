90420f514aaadfa8dae2a5295153042fb82f422a
oneal.oh <oneal.oh@kakaomobility.com>
2022-08-07 14:21:47 +0900

    update: TemplateMethod, 패턴 v4 적용의 이점

--- src/main/java/hello/advanced/app/v4/OrderControllerV4.java
+++ src/main/java/hello/advanced/app/v4/OrderControllerV4.java
@@ -15,6 +15,12 @@ public class OrderControllerV4 {
     private final OrderServiceV4 orderService;
     private final LogTrace trace;
 
+    /**
+     * V4 는 단순히 템플릿 메서드 패턴을 적용해서 소스코드 몇줄을 줄인 것이 전부가 아니다.
+     * 로그를 남기는 부분에 단일 책임 원칙(SRP)을 지킨 것이다. 변경 지점을 하나로 모아서 변경에 쉽게 대처할 수 있는 구조를 만든 것이다.
+     * @param itemId
+     * @return
+     */
     @GetMapping("/v4/request")
     public String request(String itemId) {
 
